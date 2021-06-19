73a47bf2d78c771082ad8149f1e93ad82915f1e1
Deepak Prajapati <dp0774138@gmail.com>
2021-06-19 10:54:11 +0530

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app1/pages/loginPage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'pages/HomePage.dart';
 
@@ -12,7 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      theme: ThemeData(fontFamily: GoogleFonts.lato().fontFamily),
+      routes: {
+        "/": (context) => loginPage(),
+      },
+      //home: HomePage(),
     );
   }
 }
--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -6,7 +6,47 @@ class loginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(child: Text('Login Page')),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            SizedBox(height: 20.0),
+            Image.asset("assets/images/image1.png", fit: BoxFit.cover),
+            SizedBox(height: 30.0),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter name",
+                      labelText: "Name",
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("working");
+                      },
+                      child: Text("LogIn")),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
